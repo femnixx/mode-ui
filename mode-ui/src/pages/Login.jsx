c7b2e834b91252a00e871bb24d048c63c7d4b8d6
femnix <femnitality@gmail.com>
2025-07-16 22:44:17 +0700

    made some changes to the UI slicing

--- mode-ui/src/pages/Login.jsx
+++ mode-ui/src/pages/Login.jsx
@@ -3,53 +3,53 @@ import React from "react";
 const Login = () => {
   return (
     <>
-      <div className="w-screen h-screen">
-        <h1 className="mt-13 text-center flex items-center justify-center">
+      <div className="flex flex-col h-screen">
+        <p className="flex justify-center items-center mt-13 text-center text-4xl font-serif mx-14">
           Welcome back!
-        </h1>
-        <div className="mt-12">
-          <div className="flex justify-around w-full">
-            <div className="w-1/2 flex items-center justify-center">
-              <p className="">Sign in</p>
-            </div>
-            <div className="w-1/2 flex items-center justify-center">
-              <p>Sign up</p>
-            </div>
+        </p>
+        <div className="flex  justify-between w-full">
+          <div className="py-3 px-16  w-1/2 mt-12">
+            <p>Sign in</p>
           </div>
-          {/* login */}
-          <div className="mt-12 px-4 ">
-            <div className="flex">
-              <p>Email</p>
-              <p>*</p>
-            </div>
-            <div className="flex mt-2">
-              <p>image</p>
-              <p>hugeicons@gmail.com</p>
-            </div>
-            <div className="flex mt-5">
-              <p>Password</p>
-              <p>*</p>
-            </div>
-            <div className="flex mt-2">
-              <p>image</p>
-              <p>*******</p>
-            </div>
-            <div className="flex mt-3 justify-between">
-              <div className="flex">
-                <div>.</div>
-                <p>Remember me</p>
-              </div>
-              <p>Forgot password</p>
-            </div>
-            <button className="mt-8 flex text-center border-1 w-full items-center justify-center">
-              Login
-            </button>
-            <p className="flex justify-center items-end text-center">
-              By logging in, you agree to our Terms of Conditions and Privacy
-              Policy
-            </p>
+          <div className="py-3 px-16 w-1/2 mt-12">
+            <p>Sign up</p>
           </div>
         </div>
+        <div className="mx-4">
+          <div className="mt-12 gap-y-5 flex flex-col">
+            <div className="gap-y-2">
+              <div className="flex gap-x-1">
+                <p className="font-semibold">Email</p>
+                <p className="font-semibold text-red-500">*</p>
+              </div>
+              <div className="flex p-3 gap-x-3 items-center border-1 rounded-lg border-[#E5E7EB] mt-2">
+                <p>.</p>
+                <p>hugeicons@gmail.com</p>
+              </div>
+            </div>
+            <div className="gap-y-2">
+              <div className="flex gap-x-1">
+                <p className="font-semibold">Password</p>
+                <p className="font-semibold text-red-500">*</p>
+              </div>
+              <div className="flex items-center p-3 gap-x-3 border-1 border-[#E5E7EB] rounded-lg">
+                <p>.</p>
+                <p>*****</p>
+              </div>
+            </div>
+          </div>
+          <div className="flex justify-between pt-4 items-center text-[#75757E]">
+            <div className="flex gap-x-1">
+              <button className="border-1 border-[#75757E] px-1 my-1 rounded-xs"></button>
+              <p className="text-sm">Remember me</p>
+            </div>
+            <p className="text-sm">Forgot password</p>
+          </div>
+          <p className="mt-8 text-center border-1">Login</p>
+        </div>
+        <p className="text-center mt-auto mb-6">
+          By logging in, you agree to our Terms of Conditions and Privacy Policy
+        </p>
       </div>
     </>
   );
